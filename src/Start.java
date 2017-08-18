ae55877e8c503badf4c011a31e5f2fd4cb0e95c5
Marcius Bezerra <marciusbezerra@outlook.com>
2017-08-18 02:12:51 -0300

    Initial Commit

---
+++ src/Start.java
@@ -0,0 +1,13 @@
+
+public class Start {
+
+	public Start() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("ESTE É UM HELLO WORD PARA TESTE DO AMBIENTE DE DESENVOLVIMENTO JAVA");
+	}
+
+}
